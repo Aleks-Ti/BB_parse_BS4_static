391e90c45533bc267959e4b8575796d7f2d5756f
Àëåêñàíäð Òêà÷åíêî <mti61ro@yandex.ru>
2023-09-22 07:00:51 +0300

    added pars date json

--- main/parser.py
+++ main/parser.py
@@ -2,37 +2,89 @@ import requests
 from bs4 import BeautifulSoup
 import asyncio
 import tracemalloc
-import tls_client
-import aiohttp
 from requests_html import HTMLSession
-
-header = {
-    "Accept": "application/json, text/plain, */*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0",
-}
-
+from http import HTTPStatus
+from settings import HEADER, TABLE_SCHEMA
+import requests_cache
+requests_cache.install_cache('my_cache', expire_after=3600)
 session = HTMLSession()
 
-def parse_date_rings():
-    url = 'https://qgold.com/pl/Jewelry-Rings-Adjustable/'
-    response = session.get(url)
-    response.html.render(sleep=5)
-    soup = BeautifulSoup(response.html.html, 'lxml')
-    swagger = soup.find("div", {"class": "row product-list"})
-    print(swagger.text)
-    
+
+def detail(links):
+    result = {}
+    for link in links:
+        url = 'https://jewelers.services/productcore/api'
+        url = url + link + '/'
+        response = session.get(url=url, headers=HEADER)
+        if response.status_code == HTTPStatus.OK:
+            q = response.json()
+
+            product = q.get('Product')
+            for attr in product:
+                if attr == 'InStock':
+                    if product[attr] < 1:
+                        result[attr] = 'Out of Stock'
+                        break
+                if attr in TABLE_SCHEMA:
+                    result[attr] = product[attr]
+
+
+            product_detail = q.get('Specifications')
+            for attr in product_detail:
+                qwer = attr['Specification']
+                if qwer in TABLE_SCHEMA:
+                    result[qwer] = attr['Value']
+
+            product_attr_name = q.get('AttributesNamed')
+            for attr in product_attr_name:
+                qwer = attr['AttributeDescription']
+                if qwer in TABLE_SCHEMA:
+                    result[qwer] = attr['AttributeValue']
+
+
+# def detail(links):
+#     for link in links:
+#         url = 'https://qgold.com'
+#         url = url + link + '/'
+#         response = session.get(url=url)
+#         response.html.render(sleep=10)
+#         # qwas = response.html.find('div.row.info-details-wrapper', first=False)
+#         qwas = response.html.find('div.specs-table-wrapper', first=False)
+#         print(qwas)
+#         for li in qwas:
+#             li_elements = li.find('li.trow.ng-star-inserted', first=False)
+#             for li in li_elements:
+#                 print(li.text)
+#             li_elements = li.find('div.price-tag', first=False)
+#             for p in li_elements:
+#                 print(p.text)
+
 
 def parse_date_adjustable():
-
     url = 'https://qgold.com/pl/Jewelry-Rings-Adjustable/'
     response = session.get(url)
-    response.html.render(sleep=5)
+    response.html.render(sleep=10)
     special_div = response.html.find('div.row.product-list', first=True)
     if special_div:
-        print(special_div.text)
-        print(special_div.links)
+        # special_div.text
+        links = special_div.links
+        detail(links)
     else:
         print('НЕ НАЙДЕНО')
+
+
+def parse_date_rings():
+    url = 'https://qgold.com/pl/Jewelry-Rings-2·Stone-Rings/'
+    response = session.get(url)
+    response.html.render(sleep=10)
+    special_div = response.html.find('div.row.product-list', first=True)
+    if special_div:
+        # special_div.text
+        links = special_div.links
+        detail(links)
+    else:
+        print('НЕ НАЙДЕНО')
+
 
 # parse_date_rings()
 parse_date_adjustable()
--- main/settings.py
+++ main/settings.py
@@ -17,7 +17,7 @@ MIN_IN_SEC = 60
         - 5 минут * 60 = 300 секунд
 '''
 
-RETRY_PERIOD = 1
+RETRY_PERIOD = 10
 '''Период за которой происходит повторный запуск программы:
    и парс данных с сайта.
 '''
@@ -35,3 +35,48 @@ START_MESSAGE = (
     ' N - периодичность запроса к сайту:'
 )
 '''Стартовое сообщение при запуске программы.'''
+
+
+HEADER = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
+    'Authorization': '',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/json',
+    'Host': 'jewelers.services',
+    'Origin': 'https://qgold.com',
+    'Referer': 'https://qgold.com/',
+    'Sec-Ch-Ua': '"Microsoft Edge";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'Trailerid': '820242c8-4abc-4a1c-be4e-ee4c5ff9b7f1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.36'
+}
+
+TABLE_SCHEMA = (
+    'Style',  # Product
+    'MSRP',  # Product
+    'InStock',  # Product
+    'Size',  # Product
+    'Product Type',  # Specifications
+    'Jewelry Type',  # Specifications
+    'Ring Type',  # Specifications
+    'Material: Primary',  # Specifications
+    'Material: Primary - Color',  # Specifications
+    'Material: Primary - Purity',  # Specifications
+    'Sold By Unit',  # Specifications
+    'CountryOfOrigin',  # AttributesNamed
+    'Metal',  # AttributesNamed
+    'Average Weight',  # AttributesNamed
+    'Feature',  # Specifications
+    'Feature 2',  # Specifications
+    'Finish',  # Specifications
+    'Manufacturing Process',  # Specifications
+    'Profile Type',  # Specifications
+    'Ring Shape',  # Specifications
+)
+'''Структура и ключи соответствия для получаемых данных из json.'''
